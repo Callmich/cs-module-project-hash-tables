1837af5fd12b00b1f646837d9aab7569f3068e63
Michael Callahan <CallahanMichaelE@gmail.com>
2020-08-06 13:57:12 -0600

    dictionary is now filled up

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,4 +1,11 @@
 import random
+# 1. Read the file `input.txt` and split it into words.
+# 2. Analyze the text, building up the dataset of which words can follow particular words.
+# 3. Choose a random "start word" to begin.
+# 4. Loop through: Print the word - If it's a "stop word", stop - Else randomly choose a word that can follow this one.
+
+# Start words are words that begin with a capital, or a `"` followed by a capital.
+
 
 # Read in all the words in one go
 with open("input.txt") as f:
@@ -6,6 +13,18 @@ with open("input.txt") as f:
 
 # TODO: analyze which words can follow other words
 # Your code here
+
+split_words = f.split()
+
+word_dict = {}
+
+for x in range (len(split_words[:-1])):
+    if not word_dict.get(split_words[x]):
+        word_dict[split_words[x]] = [split_words[x+1]]
+    else:
+        word_dict[split_words[x]] += [split_words[x + 1]]
+
+print(word_dict)
 
 
 # TODO: construct 5 random sentences
